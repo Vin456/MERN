2163fa69f6f076db91f233c40193ea2d51c12f2a
Vinyas R <vinyasravi@gmail.com>
2023-07-18 23:23:17 +0530

    styles added

--- codail/index.js
+++ codail/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// extract styles and script
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use(expressLayouts);
 
 app.use('/', require('./routes'));
 
